f126f7d8cd56571a7c0b654f4ebc4817d639df5b
Kenny Lei <kennyzlei@gmail.com>
2013-04-25 20:34:04 -0400

    Added Project 2 Files

---
+++ Website/script/main.js
@@ -0,0 +1,195 @@
+var loc = [0,0];
+var selectedTitle = "";
+
+/* onload */
+window.onload = function() {
+	document.getElementById("xselect").style.left = (window.innerWidth / 2).toString()+"px";
+	document.getElementById("yselect").style.top = (window.innerHeight / 2).toString()+"px";
+	document.getElementById("xselect").style.top = (window.innerHeight - 50).toString()+"px";
+	document.getElementById("vidlink").style.left = (window.innerWidth - 157).toString()+"px";
+	document.getElementById("description").style.top = "75px";
+	
+	if (window.Event) {
+		document.captureEvents(Event.MOUSEMOVE);
+	}
+
+	document.onmousemove = getCursorXY;
+
+	drawSVG();
+	document.getElementById("xAxisSelect").onchange = function(){
+		drawSVG();
+	}
+	document.getElementById("yAxisSelect").onchange = function(){
+		drawSVG();
+	}
+}
+
+function search(){
+	var a = document.getElementById("searchbox").value;
+	//Filter data to be displayed/used
+	alert("Search");
+
+}
+
+function drawSVG(){
+	d3.select("body").select("svg").remove();
+	var xAxisSelect = document.getElementById("xAxisSelect").value;
+	var yAxisSelect = document.getElementById("yAxisSelect").value;
+
+	var margin = {top: 75, right: 200, bottom: 20, left: 20},
+    width = window.innerWidth - margin.left - margin.right - 30,
+    height = window.innerHeight - 160 - margin.top - margin.bottom;
+
+	var parseDate = d3.time.format("%x").parse;
+	
+	var csvdata = document.getElementById("csv").value;
+	csvsplit = csvdata.split("\n");
+	for (j=1; j<csvsplit.length; j++)
+	{
+			csvsplit[j] = csvsplit[j].split(",");
+			if (csvsplit[j].length == 14)
+			{
+					csvsplit[j][0] = csvsplit[j][0]+csvsplit[j][1];
+					csvsplit[j].splice(1,1);
+			}
+	}
+
+	var xindex, yindex;
+
+	switch(xAxisSelect) {
+		case "time": xindex = 1; break;
+		case "ms": xindex = 6; break;
+		case "ur": xindex = 7; break;
+		case "nor": xindex = 8; break;
+		case "budget": xindex = 9; break;
+		case "boxoff": xindex = 10; break;
+		case "date": xindex = 12; break;
+	}
+
+	switch(yAxisSelect) {
+		case "time": yindex = 1; break;
+		case "ms": yindex = 6; break;
+		case "ur": yindex = 7; break;
+		case "nor": yindex = 8; break;
+		case "budget": yindex = 9; break;
+		case "boxoff": yindex = 10; break;
+		case "date": yindex = 12; break;
+	}
+
+	var data = csvsplit.slice(1,csvsplit.length-2);
+
+	for (j=0; j<data.length; j++)
+	{
+		data[j][1] = parseInt(data[j][1]);
+		data[j][6] = parseInt(data[j][6]);
+		data[j][7] = parseFloat(data[j][7]);
+		data[j][8] = parseInt(data[j][8]);
+		data[j][9] = parseInt(data[j][9]);
+		data[j][10] = parseInt(data[j][10]);
+		data[j][12] = parseDate(data[j][12]);
+	}
+
+	if (xAxisSelect != "date")
+	{
+			data = data.sort(function(a,b) {return a[xindex]-b[xindex]});
+			var x = d3.scale.linear()
+					.domain(d3.extent(data, function(d) { return d[xindex];}))
+					.range([0, width]);
+	}
+	else	
+	{
+		data = data.sort(function(a,b) {return a[xindex]-b[xindex]});
+		x = d3.time.scale()
+			.domain(d3.extent(data, function(d) { return d[xindex];}))
+	    	.range([0, width]);
+	}
+
+	if (yAxisSelect == "time")
+	{
+		var y = d3.scale.linear()
+			.domain(d3.extent(data, function(d) { return d[yindex];}))
+			.range([height, 0]);
+	}
+	else
+	{
+		var y = d3.scale.linear()
+			.domain([0, d3.extent(data, function(d) { return d[yindex];})[1]])
+			.range([height, 0]);
+	}
+		
+	var line = d3.svg.line()
+		.x(function(d,i) { return x(d[xindex])})
+		.y(function(d) { return y(d[yindex])});
+
+	var graph = d3.select("#graph").append("svg:svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		.append("svg:g")
+			.attr("transform", "translate(" + margin.right + "," + margin.top + ")");
+			
+	var xAxis = d3.svg.axis().scale(x).orient("bottom");
+	graph.append("svg:g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis);
+
+	var yAxisLeft = d3.svg.axis().scale(y).orient("left");
+	graph.append("svg:g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(-25,0)")
+			.call(yAxisLeft);	
+			
+	graph.append("svg:path").attr("class","path").attr("d", line(data));
+		
+	graph.selectAll()
+		.data(data)
+	.enter().append("svg:circle")
+		.attr("class", function(d) 
+						{
+							if (d[0] == selectedTitle)
+							{
+								return "highlighted_point";
+							} 
+							else
+							{
+								return "point";
+							}
+						})	
+		.attr("cx", function(d,i) { return x(d[xindex]);})
+		.attr("cy", function(d) { return y(d[yindex]);})
+		.attr("r", function(d) {
+							if (d[0] == selectedTitle) 
+							{
+								return 3;
+							}
+							else
+							{
+								return 2;
+							}
+						})
+		.on('mouseover', function(d){
+			d3.select(this).attr('r', 3);
+			document.getElementById("tooltip").style.position = "absolute";
+			document.getElementById("tooltip").style.top = loc[1].toString()+"px";
+			document.getElementById("tooltip").style.left = loc[0].toString()+"px";
+			document.getElementById("tooltip").style.visibility = "visible";
+			var newhtml = d[0]+"<br>x: "+d[xindex].toString()+"<br>y: "+d[yindex].toString();
+
+			document.getElementById("specificcontent").innerHTML = newhtml;
+		})
+		.on('mouseout', function(){
+			d3.select(this).attr('r', 2);
+			document.getElementById("tooltip").style.visibility = "hidden";
+		})
+		.on("click", function(d){
+			graph.selectAll(".highlighted_point").attr("class", "point")
+									.attr("r", 2);
+			d3.select(this).attr("class", "highlighted_point");
+			selectedTitle = d[0];
+		});
+}
+
+function getCursorXY(e) {
+	loc[0] = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+	loc[1] = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+}
